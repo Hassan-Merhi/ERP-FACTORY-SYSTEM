abb00fb1dbe3f8d2c301e175ab407f4bbe68b712
Hassan Dakik <hassandakik@my.uopeople.edu>
2026-09-12 12:41:26 -0500

    style: format backend coverage stabilization tests

--- tests/backend-coverage-factory-validation-writes.test.ts
+++ tests/backend-coverage-factory-validation-writes.test.ts
@@ -166,8 +166,12 @@ beforeAll(async () => {
 }, 120000);
 
 afterAll(async () => {
-  await pool.query("DELETE FROM factory_production_plans WHERE company_id = $1", [emptyFactoryCompanyId]).catch(() => undefined);
-  await pool.query("DELETE FROM factory_bale_products WHERE company_id = $1", [emptyFactoryCompanyId]).catch(() => undefined);
+  await pool
+    .query("DELETE FROM factory_production_plans WHERE company_id = $1", [emptyFactoryCompanyId])
+    .catch(() => undefined);
+  await pool
+    .query("DELETE FROM factory_bale_products WHERE company_id = $1", [emptyFactoryCompanyId])
+    .catch(() => undefined);
   await cleanupTestData(TEST_PREFIX);
   closeTestServer();
 }, 120000);
