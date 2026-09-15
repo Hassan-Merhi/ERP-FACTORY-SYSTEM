b393f4d23ae181634759af0e9c3430446b418a15
Hassan Merhi Dakik <hassandakik@my.uopeople.edu>
2026-09-15 04:23:50 -0500

    fix: register PO edit concurrency guard

--- server/routes/containers/containerFreightRoutes.ts
+++ server/routes/containers/containerFreightRoutes.ts
@@ -1,8 +1,10 @@
 import type { Express } from "express";
 import { registerContainerFreightReadRoutes } from "./containerFreightReadRoutes";
+import { registerPurchaseOrderEditConcurrencyGuard } from "./purchaseOrderEditConcurrencyGuard";
 import { registerContainerFreightWriteRoutes } from "./containerFreightWriteRoutes";
 
 export function registerContainerFreightRoutes(app: Express) {
   registerContainerFreightReadRoutes(app);
+  registerPurchaseOrderEditConcurrencyGuard(app);
   registerContainerFreightWriteRoutes(app);
 }
