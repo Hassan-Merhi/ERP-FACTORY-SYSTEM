1c90dd9533384617dc2e5cffc81e55413279d66b
Hassan Merhi Dakik <hassandakik@my.uopeople.edu>
2026-09-23 13:58:02 +0300

    Rebase Wave 2: server/i18n/factoryStaffTrackingMessages.ts

--- server/i18n/factoryStaffTrackingMessages.ts
+++ server/i18n/factoryStaffTrackingMessages.ts
@@ -1,7 +1,7 @@
 export const factoryStaffTrackingMessages = {
   noFactoryCompany: "No factory company selected",
   invalidPeriod: "Invalid tracking period",
-  forbiddenTab: "You do not have access to this Stock Entry tab",
+  forbiddenTab: "You do not have access to this Factory tracking tab",
   invalidRecordCount: "records must contain between 1 and 500 rows",
   invalidRow: "Invalid tracking row",
   personOutsideFactory: "Person does not belong to this factory company",
