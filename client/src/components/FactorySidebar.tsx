82b9ed2b9ee36054ce32d3629dfe10a516aa79bd
Hassan Merhi Dakik <hassandakik@my.uopeople.edu>
2026-09-23 15:28:18 +0300

    Wave 3: filter Factory Recent by access

--- client/src/components/FactorySidebar.tsx
+++ client/src/components/FactorySidebar.tsx
@@ -284,7 +284,7 @@ export function FactorySidebar({
   const { openSections, toggleSection } = useOpenSections(visibleSections);
 
   const allNavItems = useMemo(() => [...FACTORY_PINNED_DEFAULTS, ...FACTORY_NAV_SECTIONS.flatMap((s) => s.items)], []);
-  const recentItems = useRecentNav(allNavItems, selectedCompany?.id);
+  const recentItems = useRecentNav(allNavItems, selectedCompany?.id, isPinnedVisible);
   const visibleRecentItems = recentItems.filter(isPinnedVisible);
   const conflictsVisible = isPinnedVisible({ url: "/factory/conflicts" });
 
