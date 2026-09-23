447c61288e1b1389a774c13a99bb993398eca308
Hassan Merhi Dakik <hassandakik@my.uopeople.edu>
2026-09-23 14:31:41 +0300

    Stabilize hidden row preference hydration

--- client/src/pages/transactionjournal/useTransactionJournalModel.ts
+++ client/src/pages/transactionjournal/useTransactionJournalModel.ts
@@ -104,7 +104,7 @@ export function useTransactionJournalModel() {
         )
       )
     );
-  }, [userPreferences]);
+  }, [userPreferences?.hiddenTransactionJournalVoucherIds]);
 
   const persistHiddenRows = useCallback(
     (ids: Set<number>) => {
