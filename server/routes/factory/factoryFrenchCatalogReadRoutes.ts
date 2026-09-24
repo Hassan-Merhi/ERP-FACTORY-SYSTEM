09287eab969a43057a6ca361a9a57632f16d7596
Hassan Merhi Dakik <hassandakik@my.uopeople.edu>
2026-09-24 10:01:02 +0300

    Finish server search normalization cleanup

--- server/routes/factory/factoryFrenchCatalogReadRoutes.ts
+++ server/routes/factory/factoryFrenchCatalogReadRoutes.ts
@@ -1,6 +1,6 @@
 import { punctuationInsensitiveSearch } from "../../lib/searchNormalization";
 import type { Express, Request } from "express";
-import { and, asc, eq, ilike, isNull, or } from "drizzle-orm";
+import { and, asc, eq, isNull, or } from "drizzle-orm";
 import { factoryBaleProducts, factoryCategories } from "@shared/schema/factoryTrilingualCatalogTables";
 import { resolveFactoryCategoryName, resolveFactoryProductLanguage } from "@shared/factoryBilingualContract";
 import { requireAuth } from "../../auth";
